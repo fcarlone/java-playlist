ea4b1e3314146b4e7e0424bea483a408e7f5efb4
fcarlone <frankcarlone@gmail.com>
2020-02-24 18:56:28 -0500

    Create Song Class

---
+++ src/Song.java
@@ -0,0 +1,22 @@
+public class Song {
+    private String songName;
+    private double songDuration;
+
+//  Constructor
+    public Song(String songName, double songDuration) {
+        this.songName = songName;
+        this.songDuration = songDuration;
+    }
+
+    public String getSongName() {
+        return songName;
+    }
+
+    @Override
+    public String toString() {
+        return "Song{" +
+                "songName='" + songName + '\'' +
+                ", songDuration=" + songDuration +
+                '}';
+    }
+}
